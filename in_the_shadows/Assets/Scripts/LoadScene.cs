992d194f5be794bfbf614ef28093e183b019f6e0
Antonio Sviridov <ant.svirido@gmail.com>
2019-04-16 17:20:10 +0300

    push skeleton

---
+++ in_the_shadows/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+
+    public string scene;
+    // Update is called once per frame
+    public void onClick()
+    {
+        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+    }
+}
